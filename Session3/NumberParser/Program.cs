b259ab937f17537b1b0782239f2ea4574dd781aa
Chris Arnold <chris.arnold@nmtafe.wa.edu.au>
2026-08-04 20:03:22 +0800

    times table

--- Session3/NumberParser/Program.cs
+++ Session3/NumberParser/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-int mangosteensInPile = ReadNumber("How many mangosteens in a pile?");
-Console.WriteLine(mangosteensInPile + " mangosteens in a pile.");
-int turmericInPile = ReadNumber("How many tumeric roots in a stack?");
-Console.WriteLine(turmericInPile + " tumerics in a stack.");
+//int mangosteensInPile = ReadNumber("How many mangosteens in a pile?");
+//Console.WriteLine(mangosteensInPile + " mangosteens in a pile.");
+//int turmericInPile = ReadNumber("How many tumeric roots in a stack?");
+//Console.WriteLine(turmericInPile + " tumerics in a stack.");
 
 int ReadNumber(string prompt) {
     Console.Write(prompt.Trim() + " ");
@@ -16,6 +16,25 @@ int ReadNumber(string prompt) {
     } else {
         return int.MinValue;
     }
+}
+
+int glob = 0;
+List<int> listOfInts = new List<int>();
+
+doBadStuffs(listOfInts);
+Console.WriteLine(listOfInts.Count());
+
+//doBadStuff(glob);
+//Console.WriteLine(glob);
+void doBadStuff(int thingo) {
+    ++thingo;
+    ++glob;
+    Console.WriteLine(thingo);
+}
+void doBadStuffs(List<int> thingo) {
+    thingo.Add(1);
+    listOfInts.Add(2);
+    Console.WriteLine(thingo.Count);
 }
 
 //int ReadNumber(string prompt, out bool readingWentOK) {
---
+++ Session3/TimesTable/Program.cs
@@ -0,0 +1,29 @@
+﻿int baseNumber = -1;
+int minNumber = 1;
+int maxNumber = 12;
+bool IsInRange(int number, int min, int max) {
+    return number >= min && number <= max;
+}
+
+while (IsInRange(baseNumber,minNumber,maxNumber)) {
+    baseNumber = ReadNumber("Times table for which number would you like? ");
+    if (IsInRange(baseNumber, minNumber, maxNumber)) {
+        Console.WriteLine("No bueno! Try again");
+    }
+}
+
+for (int i = 1; i <= 12 ; ++i) {
+    Console.WriteLine($"{i} x {baseNumber} = {(i * baseNumber)}");
+}
+
+int ReadNumber(string prompt) {
+    Console.Write(prompt.Trim() + " ");
+    string userInput = Console.ReadLine();
+    int result;
+    bool parsedOK = int.TryParse(userInput, out result);
+    if (parsedOK) {
+        return result;
+    } else {
+        return int.MinValue;
+    }
+}
