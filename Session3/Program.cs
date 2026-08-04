0f6589f3b88e06de1835fbb5b3b5eb69cf996e0f
Chris Arnold <chris.arnold@nmtafe.wa.edu.au>
2026-08-04 18:28:52 +0800

    First example (input)

---
+++ Session3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Hello, World!");
+Console.Write("How many bananas in a pile? ");
+string input = Console.ReadLine();
+
+Console.WriteLine($"There are {input} inputs in a line.");
